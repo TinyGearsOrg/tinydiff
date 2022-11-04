07d68ef104e2b30bf489ade847c1358975fdd4a6
netomi <thomas.neidhart@gmail.com>
2022-11-04 22:47:33 +0100

    Implement optimized version of the diff algorithm.

--- src/main/kotlin/org/tinygears/tinydiff/algorithm/OptimizedDiffAlgorithm.kt
+++ src/main/kotlin/org/tinygears/tinydiff/algorithm/OptimizedDiffAlgorithm.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Thomas Neidhart.
+ * Copyright 2022 Thomas Neidhart.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,180 +15,155 @@
  */
 package org.tinygears.tinydiff.algorithm
 
-//import org.netomi.jdiffutils.transform.Transformer
-//
-///**
-// *
-// *
-// * @param <T>
-//</T> */
-//class OptimizedDiffAlgorithm<T> : DiffAlgorithm<T> {
-//    override fun getEditScript(
-//        origSequence: List<T>, newSequence: List<T>,
-//        transformer: Transformer<T>
-//    ): EditScript<T> {
-//        val map: MutableMap<T, EquivalenceClass<T>> =
-//            HashMap()
-//        val transformedA: MutableList<T> = ArrayList(origSequence.size)
-//        for (a in origSequence) {
-//            val transformed = if (transformer == null) a else transformer.transform(a)
-//            transformedA.add(transformed)
-//            var c = map[transformed]
-//            if (c == null) {
-//                c = EquivalenceClass(transformed)
-//                map[transformed] = c
-//            }
-//            c.incA()
-//        }
-//        val transformedB: MutableList<T> = ArrayList(newSequence.size)
-//        for (b in newSequence) {
-//            val transformed = if (transformer == null) b else transformer.transform(b)
-//            transformedB.add(transformed)
-//            var c = map[transformed]
-//            if (c == null) {
-//                c = EquivalenceClass(transformed)
-//                map[transformed] = c
-//            }
-//            c.incB()
-//        }
-//        val reverseMap: Array<EquivalenceClass<T>> =
-//            arrayOfNulls<EquivalenceClass<*>>(map.size)
-//        var index = 0
-//        var countA = 0
-//        var countB = 0
-//        for ((_, c) in map) {
-//            if (c.getCountA() > 0 && c.getCountB() > 0) {
-//                c.setIndex(index)
-//                reverseMap[index] = c
-//                countA += c.getCountA()
-//                countB += c.getCountB()
-//                index++
-//            }
-//        }
-//
-//        // TODO: find better heuristic when to omit the transformation
-//        //        as the plain myers algorithm will be faster, i.e. because
-//        //        the number of estimated changes is very small
-//        if (countA + countB > origSequence.size) {
-//            return MyersDiffAlgorithm<T>().getEditScript(origSequence, newSequence, transformer)
-//        }
-//        val listA: MutableList<Int> = ArrayList(countA)
-//        val listB: MutableList<Int> = ArrayList(countB)
-//        val indexA: MutableList<Int> = ArrayList(countA)
-//        val indexB: MutableList<Int> = ArrayList(countB)
-//        var lineNo = 0
-//        for (s in transformedA) {
-//            val c = map[s]!!
-//            if (c.getIndex() >= 0) {
-//                listA.add(c.getIndex())
-//                indexA.add(lineNo)
-//            }
-//            lineNo++
-//        }
-//        lineNo = 0
-//        for (s in transformedB) {
-//            val c = map[s]!!
-//            if (c.getIndex() >= 0) {
-//                listB.add(c.getIndex())
-//                indexB.add(lineNo)
-//            }
-//            lineNo++
-//        }
-//        val diff: DiffAlgorithm<Int> = MyersDiffAlgorithm()
-//        val script = diff.getEditScript(listA, listB, null)
-//        val visitor =
-//            ReverseTransformVisitor(origSequence, newSequence, reverseMap, indexA, indexB)
-//        script.visit(visitor)
-//        return visitor.getScript()
-//    }
-//
-//    private class ReverseTransformVisitor<T>(
-//        var listA: List<T>, var listB: List<T>, var map: Array<EquivalenceClass<*>>,
-//        var indexA: List<Int>, var indexB: List<Int>
-//    ) : CommandVisitor<Int?> {
-//        var lineNoA = 0
-//        var lineNoB = 0
-//        var lastA = 0
-//        var lastB = 0
-//        var script: EditScript<T>
-//
-//        init {
-//            script = EditScript()
-//        }
-//
-//        override fun visitInsertCommand(`object`: Int) {
-//            val next = indexB[lineNoB]
-//            while (next >= lastB) {
-//                script.appendInsert(listB[lastB++])
-//            }
-//            lineNoB++
-//        }
-//
-//        override fun visitKeepCommand(`object`: Int?) {
-//            val nextA = indexA[lineNoA]
-//            val nextB = indexB[lineNoB]
-//            while (nextA > lastA) {
-//                script.appendDelete(listA[lastA++])
-//            }
-//            while (nextB > lastB) {
-//                script.appendInsert(listB[lastB++])
-//            }
-//            val c: EquivalenceClass<T> = map[`object`!!.toInt()]
-//            script.appendKeep(c.getLine())
-//            lastA++
-//            lastB++
-//            lineNoA++
-//            lineNoB++
-//        }
-//
-//        override fun visitDeleteCommand(`object`: Int) {
-//            val next = indexA[lineNoA]
-//            while (next >= lastA) {
-//                script.appendDelete(listA[lastA++])
-//            }
-//            lineNoA++
-//        }
-//
-//        fun startVisit() {}
-//        fun finishVisit() {
-//            while (lastA < listA.size) {
-//                script.appendDelete(listA[lastA++])
-//            }
-//            while (lastB < listB.size) {
-//                script.appendInsert(listB[lastB++])
-//            }
-//        }
-//    }
-//
-//    /**
-//     * A class to represent equivalence classes of input data.
-//     *
-//     *
-//     * An equivalence class corresponds to a set of input data that
-//     * is supposed to be equal based on some criteria, e.g. after
-//     * transformation of the input data to lower case.
-//     *
-//     * @param <T>
-//    </T> */
-//    private class EquivalenceClass<T>(val line: T) {
-//        var index: Int
-//        var countA: Int
-//            private set
-//        var countB: Int
-//            private set
-//
-//        init {
-//            index = -1
-//            countA = 0
-//            countB = 0
-//        }
-//
-//        fun incA() {
-//            countA++
-//        }
-//
-//        fun incB() {
-//            countB++
-//        }
-//    }
-//}
+import org.tinygears.tinydiff.transform.Transformer
+import org.tinygears.tinydiff.transform.transform
+
+/**
+ * An optimized version of a [DiffAlgorithm].
+ *
+ * The runtime complexity of the Myers Diff algorithm is O(ND) which means if there are a great
+ * number of differences, it becomes quadratic. The optimized version tries to circumvent that
+ * by only generating the optimal edit script for elements of the sequences that are present in
+ * both. The elements that are only present in the original or modified sequence will then be
+ * inserted or deleted separately to speed up the computation.
+ */
+internal class OptimizedDiffAlgorithm<T>: DiffAlgorithm<T> {
+    override fun getEditScript(origSequence: List<T>, modifiedSequence: List<T>, vararg transformers: Transformer<T>): EditScript<T> {
+
+        val inputToEquivalenceClassMap: MutableMap<T, EquivalenceClass<T>> = mutableMapOf()
+
+        val origSequenceTransformed     = origSequence.map { transform(it, *transformers) }.toList()
+        val modifiedSequenceTransformed = modifiedSequence.map { transform(it, *transformers) }.toList()
+
+        val mapEquivalenceClass: (List<T>, (EquivalenceClass<T>) -> Unit) -> Unit = { sequence, func ->
+            for (obj in sequence) {
+                func(inputToEquivalenceClassMap.computeIfAbsent(obj) { key -> EquivalenceClass(key) })
+            }
+        }
+
+        mapEquivalenceClass.invoke(origSequenceTransformed)     { c -> c.countA++ }
+        mapEquivalenceClass.invoke(modifiedSequenceTransformed) { c -> c.countB++ }
+
+        val reverseMapping: Array<EquivalenceClass<T>?> = arrayOfNulls(inputToEquivalenceClassMap.size)
+
+        var index  = 0
+        var countA = 0
+        var countB = 0
+
+        for ((_, equivalenceClass) in inputToEquivalenceClassMap) {
+            if (equivalenceClass.isPresentInBothSequences) {
+                equivalenceClass.index = index
+                reverseMapping[index++] = equivalenceClass
+                countA += equivalenceClass.countA
+                countB += equivalenceClass.countB
+            }
+        }
+
+        val origSequenceEquivalence     = ArrayList<Int>(countA)
+        val modifiedSequenceEquivalence = ArrayList<Int>(countB)
+
+        val indexToLineOrig     = ArrayList<Int>(countA)
+        val indexToLineModified = ArrayList<Int>(countB)
+
+        val createReverseMapping: (List<T>, MutableList<Int>, MutableList<Int>) -> Unit = { input, output, mapping ->
+            for ((lineNumber, obj) in input.withIndex()) {
+                val equivalenceClass = inputToEquivalenceClassMap[obj]!!
+                if (equivalenceClass.index >= 0) {
+                    output.add(equivalenceClass.index)
+                    mapping.add(lineNumber)
+                }
+            }
+        }
+
+        createReverseMapping(origSequenceTransformed, origSequenceEquivalence, indexToLineOrig)
+        createReverseMapping(modifiedSequenceTransformed, modifiedSequenceEquivalence, indexToLineModified)
+
+        val script  = MyersDiffAlgorithm<Int>().getEditScript(origSequenceEquivalence, modifiedSequenceEquivalence)
+        val visitor = ReverseTransformVisitor(origSequence, modifiedSequence, reverseMapping, indexToLineOrig, indexToLineModified)
+        script.accept(visitor)
+        return visitor.script
+    }
+}
+
+/**
+ * A class to represent equivalence classes of input data.
+ *
+ * An equivalence class corresponds to a set of input data that
+ * is supposed to be equal based on some criteria, e.g. after
+ * transformation of the input data to lower case.
+ */
+private class EquivalenceClass<T>(val line: T) {
+    var index: Int = -1
+
+    var countA: Int = 0
+    var countB: Int = 0
+
+    val isPresentInBothSequences: Boolean
+        get() = countA > 0 && countB > 0
+
+    override fun toString(): String {
+        return "EquivalenceClass[$line]"
+    }
+}
+
+private class ReverseTransformVisitor<T>(var listA:  List<T>,
+                                         var listB:  List<T>,
+                                         var map:    Array<EquivalenceClass<T>?>,
+                                         var indexA: List<Int>,
+                                         var indexB: List<Int>) : CommandVisitor<Int> {
+    var lineNoA = 0
+    var lineNoB = 0
+
+    var lastA = 0
+    var lastB = 0
+
+    var script: EditScript<T> = EditScript.empty()
+
+    override fun visitInsertCommand(obj: Int) {
+        val next = indexB[lineNoB]
+        while (next >= lastB) {
+            script.appendInsert(listB[lastB++])
+        }
+        lineNoB++
+    }
+
+    override fun visitKeepCommand(origObj: Int?, modifiedObj: Int?) {
+        val nextA = indexA[lineNoA]
+        val nextB = indexB[lineNoB]
+        while (nextA > lastA) {
+            script.appendDelete(listA[lastA++])
+        }
+        while (nextB > lastB) {
+            script.appendInsert(listB[lastB++])
+        }
+
+        val objA = listA[indexA[lineNoA]]
+        val objB = listB[indexB[lineNoB]]
+
+        script.appendKeep(objA, objB)
+
+        lastA++
+        lastB++
+        lineNoA++
+        lineNoB++
+    }
+
+    override fun visitDeleteCommand(obj: Int) {
+        val next = indexA[lineNoA]
+        while (next >= lastA) {
+            script.appendDelete(listA[lastA++])
+        }
+        lineNoA++
+    }
+
+    override fun visitStart() {}
+
+    override fun visitEnd() {
+        while (lastA < listA.size) {
+            script.appendDelete(listA[lastA++])
+        }
+        while (lastB < listB.size) {
+            script.appendInsert(listB[lastB++])
+        }
+    }
+}
